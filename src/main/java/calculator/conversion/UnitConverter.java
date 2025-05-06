f6feaa67675de49c064a10b10275aea97169847e
Le-Mael <maelnjopmou50@gmail.com>
2025-05-06 02:16:26 +0200

    start conversion units

---
+++ src/main/java/calculator/conversion/UnitConverter.java
@@ -0,0 +1,54 @@
+package calculator.conversion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnitConverter {
+
+    private static final List<Units> unitList = new ArrayList<>();
+
+    static {
+        // Length units (base: meter)
+        unitList.add(new Units("m", "length", 1.0));
+        unitList.add(new Units("cm", "length", 100.0));
+        unitList.add(new Units("km", "length", 0.001));
+        unitList.add(new Units("ft", "length", 3.28084));
+        unitList.add(new Units("in", "length", 39.3701));
+
+        // Weight units (base: kilogram)
+        unitList.add(new Units("kg", "weight", 1.0));
+        unitList.add(new Units("g", "weight", 1000.0));
+        unitList.add(new Units("lb", "weight", 2.20462));
+        unitList.add(new Units("ton", "weight", 0.001));
+
+        // Time units (base: second)
+        unitList.add(new Units("s", "time", 1.0));
+        unitList.add(new Units("min", "time", 1.0 / 60.0));
+        unitList.add(new Units("h", "time", 1.0 / 3600.0));
+    }
+
+    public static double convert(double value, String from, String to, String category) {
+        Units fromUnit = null, toUnit = null;
+
+        for (Units u : unitList) {
+            if (u.getCategory().equalsIgnoreCase(category)) {
+                if (u.getName().equalsIgnoreCase(from)) fromUnit = u;
+                if (u.getName().equalsIgnoreCase(to)) toUnit = u;
+            }
+        }
+
+        if (fromUnit == null || toUnit == null) {
+            throw new IllegalArgumentException("Invalid units in category: " + category);
+        }
+
+        return (value / fromUnit.getFactorToBase()) * toUnit.getFactorToBase();
+    }
+
+    // Demo main method
+    public static void main(String[] args) {
+        System.out.println("10 meters = " + convert(10, "m", "ft", "length") + " feet");
+        System.out.println("2 kilograms = " + convert(2, "kg", "lb", "weight") + " pounds");
+        System.out.println("120 seconds = " + convert(120, "s", "min", "time") + " minutes");
+    }
+}
+
