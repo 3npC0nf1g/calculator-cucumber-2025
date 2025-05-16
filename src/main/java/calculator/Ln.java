ed37b3245739970173bec64eb090d5977e2e6f08
3npC0nf1g <teumayouomo113@gmail.com>
2025-05-16 19:06:46 +0200

    fix(Ln): some quality improvement

--- src/main/java/calculator/Ln.java
+++ src/main/java/calculator/Ln.java
@@ -51,39 +51,39 @@ public final class Ln extends UnaryOperation {
      */
     @Override
     public NumericValue op(NumericValue val) {
-        // Si c'est un nombre complexe, on peut calculer son logarithme naturel
+
         if (val instanceof ComplexValue) {
             return val.ln();
         }
 
-        // Pour les nombres réels, on vérifie qu'ils sont strictement positifs
+
         if (val.getValueInt() <= 0) {
             throw new ArithmeticException("Cannot calculate natural logarithm of non-positive number");
         }
 
-        // Traitement unifié pour les RealValue
+
         if (val instanceof RealValue rv) {
             double x = rv.getValue().doubleValue();
 
-            // Cas spécial pour e²
+
             if (Math.abs(x - (Math.E * Math.E)) < 0.000001) {
-                // Pour ln(e²), retourner exactement 2
+
                 BigDecimal two = BigDecimal.valueOf(2.0);
                 return new RealValue(two.doubleValue(), rv.getPrecision());
             }
 
-            // Calculer le logarithme
+
             double res = Math.log(x);
 
-            // Cas spécial pour ln(2) (résultat de ln(e²))
+
             if (Math.abs(res - 2.0) < 0.000001) {
-                // Pour ln(ln(e²)), retourner exactement 1
+
                 BigDecimal one = BigDecimal.valueOf(1.0);
                 return new RealValue(one.doubleValue(), rv.getPrecision());
             }
 
-            // Pour tous les autres cas, utiliser BigDecimal pour la précision
-            BigDecimal result = new BigDecimal(res, new MathContext(rv.getPrecision()));
+            BigDecimal result = new BigDecimal(String.valueOf(res), new MathContext(rv.getPrecision()));
+
             result = result.setScale(6, RoundingMode.HALF_UP);
 
             return new RealValue(result.doubleValue(), rv.getPrecision());
